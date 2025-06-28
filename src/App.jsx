4a55f2dae035dd61c7d3050a42a3c963b67c8608
Timothy Akobundu <106971221+timothy2462@users.noreply.github.com>
2025-06-28 22:36:08 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
-// "Explain things like you would to a 10 year old learning how to code."
-const systemMessage = { //  Explain things like you're talking to a software professional with 5 years of experience.
+
+const systemMessage = { 
   "role": "system", "content": "Explain things like you're talking to a software professional with 2 years of experience."
 }
 
@@ -52,8 +52,8 @@ function App() {
     const apiRequestBody = {
       "model": "gpt-3.5-turbo",
       "messages": [
-        systemMessage,  // The system message DEFINES the logic of our chatGPT
-        ...apiMessages // The messages from our chat with ChatGPT
+        systemMessage,  
+        ...apiMessages 
       ]
     }
 
